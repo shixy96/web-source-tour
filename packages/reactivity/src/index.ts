03deb3e80a928a91cb99233419bc1821209177d4
施翔宇 <shixy@feifanuniv.com>
2022-11-06 19:13:42 +0800

    feat: project init

---
+++ packages/reactivity/src/index.ts
@@ -0,0 +1,6 @@
+// @todo
+// vue的响应式实现
+import { isObject } from '@shengxj/utils'
+
+export const ret = isObject({})
+// console.log(ret)
---
+++ packages/utils/__test__/index.test.ts
@@ -0,0 +1,19 @@
+
+import {describe, it, expect} from 'vitest'
+import {isObject,isOn} from '../src'
+
+describe('测试工具库',()=>{
+  it('测试isObject函数',()=>{
+    expect(1+2).toBe(3)
+    expect(isObject({})).toBe(true)
+    expect(isObject(1)).toBe(false)
+    expect(isObject(null)).toBe(false)
+    expect(isObject(undefined)).toBe(false)
+    expect(isObject('')).toBe(false)
+    expect(isObject([])).toBe(true)
+  })
+  it('测试isOn函数',()=>{
+    expect(isOn('onClick')).toBe(true)
+    expect(isOn('click')).toBe(false)
+  })
+})
